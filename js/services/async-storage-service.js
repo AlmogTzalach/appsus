5b15edf29f365fd571c094f1c9143c138436a2cb
Almog Tzalach <almogtza@gmail.com>
2022-06-30 17:06:34 +0300

    Small change in storage service (push to unshift)

--- js/services/async-storage-service.js
+++ js/services/async-storage-service.js
@@ -26,7 +26,7 @@ function get(entityType, entityId) {
 function post(entityType, newEntity) {
 	newEntity.id = utilService.makeId()
 	return query(entityType).then(entities => {
-		entities.push(newEntity)
+		entities.unshift(newEntity)
 		_save(entityType, entities)
 		return newEntity
 	})
