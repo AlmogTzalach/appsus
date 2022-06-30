551a0a31609cfda8a92264c70cb44e0d971de15a
Anton Kandyba <antonkandyba@gmail.com>
2022-06-30 16:59:42 +0300

    fixed bugs, added demo data

--- js/apps/mail-app/cmps/mail-list.js
+++ js/apps/mail-app/cmps/mail-list.js
@@ -1,12 +1,14 @@
 import mailPreview from './mail-preview.cmp.js'
-import { mailService } from '../services/mail.service.js'
 
 export default {
 	template: `
         <section v-if="mails" class="">
             <ul class="mail-list clean-list">
-                <li v-for="mail in mails ">
-                    <mail-preview :mail="mail" @click="openMail(mail.id)" />
+                <li v-for="mail in mails" :key="mail.id">
+                    <mail-preview :mail="mail"
+						@click="openMail(mail.id)"
+						@starred="toggleStar"
+					/>
                 </li>
             </ul>
         </section>
@@ -21,6 +23,14 @@ export default {
 	methods: {
 		openMail(id) {
 			this.$router.push(`/mail/${this.$route.params.status}/${id}`)
+
+			// if an unread mail is opened, change its read status
+			if (!this.mails.find((mail) => mail.id === id).isRead) {
+				this.$emit('opened', id)
+			}
+		},
+		toggleStar(id) {
+			const mail = this.$emit('starred', id)
 		},
 	},
 	computed: {},
