348f95c39d0f2b0598c70a7e97eb97d1a9e9568a
Almog Tzalach <almogtza@gmail.com>
2022-07-02 16:56:01 +0300

    Added homepage images

--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -1,7 +1,21 @@
 export default {
 	template: `
         <section class="home-page app-main">
-			<h3>Home sweet home</h3>
+			<h2>All your favorite apps in one place.</h2>
+			<div class="home-page-img-container flex space-around">
+				<div class="img-container">
+					<img src="./assets/notes-homepage.svg">
+					<h3>Notes</h3>
+				</div>
+				<div class="img-container">
+					<img src="./assets/mail-homepage.svg">
+					<h3>Mail</h3>
+				</div>
+				<div class="img-container">
+					<img src="./assets/book-homepage.svg">
+					<h3>Books</h3>
+				</div>
+			</div>
 		</section>
 	  `,
 
