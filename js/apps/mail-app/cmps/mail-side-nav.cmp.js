c35039c41c8ae22495a3035c16cbdc97549e10bc
Anton Kandyba <antonkandyba@gmail.com>
2022-07-02 22:07:38 +0300

    deleted drafts row

--- js/apps/mail-app/cmps/mail-side-nav.cmp.js
+++ js/apps/mail-app/cmps/mail-side-nav.cmp.js
@@ -5,7 +5,6 @@ export default {
             <div><router-link to="/mail/inbox"><span class="fa-solid fa-inbox" /> {{ inboxText }}</router-link></div>
             <div><router-link to="/mail/starred"><span class="fa-solid fa-star" />Starred</router-link></div>
             <div><router-link to="/mail/sent"><span class="fa-solid fa-paper-plane" />Sent</router-link></div>
-            <div><router-link to="/mail/drafts"><span class="fa-solid fa-file" />Drafts</router-link></div>
             <div><router-link to="/mail/trash"><span class="fa-solid fa-trash" />Trash</router-link></div>
         </section>
     `,
